3acdc46e4ec72b7c6d4a8a423f65897f1bdc25cd
acarmfwd <acarmona@fwdcostarica.com>
2024-08-22 13:28:17 -0600

    Se hizo el pull y se adecuaron los cambios

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 import {updateUser,postUsers,getUsers,addUser,tries,removeUser,deleteUser} from "./services/services.js";
 
+class solicitud {
+    constructor(nombre,sede,fechaS,fechaE,codigo) {
+        this.nombre=nombre;
+        this.sede=sede;
+        this.fechaS=fechaS;
+        this.fechaE=fechaE;
+        this.codigo=codigo;
+    }
+    eliminar(){
+        
+    }
+}
