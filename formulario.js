ab3ee78ce5679c9640577c244443220a96ff8385
acarmfwd <acarmona@fwdcostarica.com>
2024-08-23 15:17:33 -0600

    Se separaron los JS en uno especifico por Html

---
+++ formulario.js
@@ -0,0 +1,27 @@
+
+import {updateU,updateUser,addUser,deleteUser,tries,getUsers,removeUser, eliminarPeticion} from "./services/services.js";
+
+
+//-----------------------------Admin------------------------------------//
+//Iniciador de clase solicitud
+class solicitud {
+    constructor(nombre,sede,fechaS,fechaE,codigo,estado) {
+        this.nombre=nombre;
+        this.sede=sede;
+        this.fechaS=fechaS;
+        this.fechaE=fechaE;
+        this.codigo=codigo;
+        this.estado="Pendiente";
+    }
+    remove(id){
+        eliminarPeticion(id)
+    }
+}
+//Obtencion de valores para la solicitud
+let nameCont=document.getElementById("nombreForm")
+nameCont.innerHTML=inputName.value;
+
+
+export {solicitud}
+
+    
