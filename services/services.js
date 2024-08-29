869de28b3f8169e7d14dccceebc10bcd196e41d2
acarmfwd <acarmona@fwdcostarica.com>
2024-08-29 10:32:00 -0600

    Se avanzo en los modales y se corrigieron errores

--- services/services.js
+++ services/services.js
@@ -16,22 +16,6 @@ async function getUsers() {
     })
 }
 
-async function tries() {
-    try {let usuarios= await getUsers();
-        console.log(usuarios);
-    } catch (error){console.log("No se pudo conectar"+error);}
-}
-
-async function findUser(username) {
-    let response= await fetch("http://localhost:3000/users/")
-    let usuarios= await response.json();
-    usuarios.forEach(async user => {
-        if (user.username==username) {
-            return await fetch ("http://localhost:3000/users/")+user.id
-        }
-    });
-}
-
 //-----------------------------------Post----------------------------------//
 async function postUsers(username,email,password,admin,sede,codigo) {
     try {
@@ -56,24 +40,8 @@ async function postUsers(username,email,password,admin,sede,codigo) {
       throw error;
     }
 }
-//Añadir usuarions si no existen aun
- async function addUser(username,email,password,codigo,sede,codigoPc) {  
-    let admin=false;
-    if (codigo=="0000") {
-        admin=true
-    } 
-    try {
-        let users= await getUsers();
-        let userExist=users.some(user=>user.username===username);
-        if (userExist) {
-            return console.log("Ya existe el usuario");    
-        }
-        else {await postUsers(username,email,password,admin,sede,codigoPc);
-        console.log("Se añadio el usuario");}
-    } catch (error) {
-        return console.log("Error en la funcion update", error);
-    }
-}
+
+
 //---------------------------------Delete----------------------------------//
 async function deleteUser(id) {
     try {
@@ -93,15 +61,6 @@ async function deleteUser(id) {
     } 
 }
 
-async function removeUser(username) {
-    let response = await fetch ("http://localhost:3000/users");
-    let users = await response.json();
-    users.forEach(async (user) => {
-        if (user.username==username) {
-            return await deleteUser(user.id) 
-        }
-    }); 
-}
 
 //----------------------------------Update---------------------------------//
 
@@ -132,18 +91,6 @@ async function updateUser(username,password,email, admin,sede,codigo,id) {
 }
 
 
-async function updateU(username,newusername) {
-    let response= await fetch("http://localhost:3000/users/")
-    let usuarios= await response.json();
-    
-    usuarios.forEach(async user => {
-        if (user.username==username) {
-            return await updateUser(newusername,user.password,user.id) 
-        }
-    });
-}
-
-
 //--------------------------------------Peticiones------------------------------------------------//
 
 //----------------------------------------------Get-----------------------------------------------//
@@ -159,13 +106,6 @@ async function getPeticiones() {
         }
     })
 }
-
-async function tryPeticion() {
-    try {let peticiones= await getPeticiones();
-        console.log(peticiones);
-    } catch (error){console.log("No se pudo conectar"+error);}
-}
-
 
 //-----------------------------------------------Post-----------------------------------------------//
 async function postPeticiones(userData) {
@@ -205,7 +145,7 @@ async function eliminarPeticion(id) {
 }
 
 //-------------------------------------Update-----------------------------------------//
-import { solicitud } from "../formulario.js";
+import { solicitud } from "/Js/formulario.js";
 async function updatePeticion(nombre,practica,sede,fechaS,fechaE,codigo,estado,id) {
     let peticion= new solicitud(nombre,practica,sede,fechaS,fechaE,codigo)
     peticion.id=id; peticion.estado=estado;
@@ -225,15 +165,4 @@ async function updatePeticion(nombre,practica,sede,fechaS,fechaE,codigo,estado,i
     }
 }
 
-async function updateP(username,newusername) {
-    let response= await fetch("http://localhost:3000/users/")
-    let usuarios= await response.json();
-    usuarios.forEach(async user => {
-        if (user.username==username) {
-            return await updateUser(newusername,user.password,user.id) 
-        }
-    });
-}
-
-
-export {updateU,postUsers,updateUser,addUser,deleteUser,tries,getUsers,removeUser,postPeticiones,tryPeticion,eliminarPeticion,getPeticiones,updatePeticion}
+export {postUsers,updateUser,deleteUser,getUsers,postPeticiones,eliminarPeticion,getPeticiones,updatePeticion}
